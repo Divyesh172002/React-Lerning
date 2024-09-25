5bc6f32bfcbf88dc94f10288bbe99a1bddf66dbb
Divyesh Umretiya <155971924+Divyesh172002@users.noreply.github.com>
2024-09-25 21:00:42 +0530

    Exercise-1

--- exercise-1.js
+++ exercise-1.js
@@ -97,7 +97,7 @@ function isPrime(num) {
     if (num <= 3) return true;
     if (num % 2 === 0 || num % 3 === 0) return false;
 
-    for (let i = 5; i * i <= num; i += 3) {
+    for (let i = 5; i * i <= num; i += 6) {
         if (num % i === 0 || num % (i + 2) === 0) return false;
     }
 
